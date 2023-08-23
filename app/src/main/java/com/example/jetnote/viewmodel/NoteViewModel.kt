5b592e90e146c22ea22a3823bce89d85f5b436bc
Kenneth Kai Zeng <kennethkai.zeng@yahooinc.com>
2023-08-23 01:45:19 -0700

    Fix empty state bug

--- app/src/main/java/com/example/jetnote/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/example/jetnote/viewmodel/NoteViewModel.kt
@@ -27,9 +27,7 @@ class NoteViewModel @Inject constructor(
             noteRepository.getAllNotes()
                 .distinctUntilChanged()
                 .collect { listOfNotes ->
-                    if (listOfNotes.isNotEmpty()) {
-                        _noteList.value = listOfNotes
-                    }
+                    _noteList.value = listOfNotes
                 }
         }
     }
